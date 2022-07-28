9c08a1c4ab6a5ebb51c23fc3d1fcb57fb7b03952
João H <joaoheldermartins@hotmail.com>
2022-07-28 19:51:02 -0300

    tentando os 100% nos testes

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,16 +1,30 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
+  const closed = 'The zoo is closed';
+  const object = {
+    Tuesday: { open: 8, close: 6 },
+    Wednesday: { open: 8, close: 6 },
+    Thursday: { open: 10, close: 8 },
+    Friday: { open: 10, close: 8 },
+    Saturday: { open: 8, close: 10 },
+    Sunday: { open: 8, close: 8 },
+    Monday: { open: 0, close: 0 },
+  };
+
   it('retorna "The zoo is closed" quando o argumento passado for "Monday e 09:00-AM"', () => {
     const count = getOpeningHours('Monday', '09:00-AM');
-    const expected = 'The zoo is closed';
-    expect(count).toEqual(expected);
+    expect(count).toEqual(closed);
   });
 
   it('retorna "The zoo is open" quando o argumento passado for "Tuesday e 09:00-AM"', () => {
     const count = getOpeningHours('Tuesday', '09:00-AM');
-    const expected = 'The zoo is open';
-    expect(count).toEqual(expected);
+    expect(count).toEqual('The zoo is open');
+  });
+
+  it('retorna "The zoo is open" quando o argumento passado for "friday e 00:00-PM"', () => {
+    const count = getOpeningHours('friday', '00:00-PM');
+    expect(count).toEqual('The zoo is open');
   });
 
   it('retorna "The zoo is closed" quando o argumento passado for "Wednesday e 09:00-PM"', () => {
@@ -30,23 +44,28 @@ describe('Testes da função getOpeningHours', () => {
     expect(count).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
   });
 
-  it('retorna uma exceção com a mensagem: "The hour should represent a number" quando o argumento passado for "Wednesday e JH09:00-PM"', () => {
-    const count = () => getOpeningHours('Wednesday', 'JH09:00-PM');
+  it('retorna uma exceção com a mensagem: "The hour should represent a number" quando o argumento passado for "Friday e JH09:00-PM"', () => {
+    const count = () => getOpeningHours('Friday', 'JH09:00-PM');
     expect(count).toThrowError(new Error('The hour should represent a number'));
   });
 
-  it('retorna uma exceção com a mensagem: "The minutes should represent a number" quando o argumento passado for "Wednesday e 09:c0-AM"', () => {
-    const count = () => getOpeningHours('Wednesday', '09:c0-AM');
+  it('retorna uma exceção com a mensagem: "The minutes should represent a number" quando o argumento passado for "Saturday e 09:c0-AM"', () => {
+    const count = () => getOpeningHours('Saturday', '09:c0-AM');
     expect(count).toThrowError(new Error('The minutes should represent a number'));
   });
 
-  it('retorna uma exceção com a mensagem: "The hour must be between 0 and 12" quando o argumento passado for "Tuesday e 13:00-AM"', () => {
-    const count = () => getOpeningHours('Tuesday', '13:00-AM');
+  it('retorna uma exceção com a mensagem: "The hour must be between 0 and 12" quando o argumento passado for "Tuesday e 13:00-PM"', () => {
+    const count = () => getOpeningHours('Tuesday', '13:00-PM');
     expect(count).toThrowError(new Error('The hour must be between 0 and 12'));
   });
 
   it('retorna uma exceção com a mensagem: "The minutes must be between 0 and 59" quando o argumento passado for "Sunday e 09:60-AM"', () => {
     const count = () => getOpeningHours('Sunday', '09:60-AM');
     expect(count).toThrowError(new Error('The minutes must be between 0 and 59'));
+  });
+
+  it('retorna um objeto com os horarios e dias quando não forem passados argumentos', () => {
+    const count = getOpeningHours('', '');
+    expect(count).toEqual(object);
   });
 });
