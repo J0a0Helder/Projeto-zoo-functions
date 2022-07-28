c3d0b5c645b9c9bbb432d2131565f2e683082627
João H <joaoheldermartins@hotmail.com>
2022-07-28 15:18:31 -0300

    requesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
 
+  return data.species.filter((parameter) => ids.includes(parameter.id));
+}
+getSpeciesByIds(data);
 module.exports = getSpeciesByIds;
