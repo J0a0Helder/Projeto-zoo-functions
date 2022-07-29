eaf1e31dc140aa72c94e9c5c2aae850a1d76f45f
João H <joaoheldermartins@hotmail.com>
2022-07-29 17:37:57 -0300

    requesito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+const { adult, senior, child } = prices;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const crianca = entrants.filter((idade) => idade.age < 18).length;
+  const adulto = entrants.filter((idade) => idade.age >= 18 && idade.age < 50).length;
+  const idoso = entrants.filter((idade) => idade.age >= 50).length;
+  const quantidades = { child: crianca, adult: adulto, senior: idoso };
+  return quantidades;
 }
 
 function calculateEntry(entrants) {
-  //
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const pessoa = countEntrants(entrants);
+  return ((pessoa.adult * adult) + (pessoa.child * child) + (pessoa.senior * senior));
 }
 
 module.exports = { calculateEntry, countEntrants };
