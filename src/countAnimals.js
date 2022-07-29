1211abd93d6a3224d075972500a71e253ae55d31
João H <joaoheldermartins@hotmail.com>
2022-07-29 16:08:05 -0300

    requesito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const animais = species.map((animalzinho) => animalzinho.name);
+const quantidade = species.map((animalzinho) => animalzinho.residents.length);
+const obj = {};
+animais.forEach((valor, index) => { obj[valor] = quantidade[index]; });
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) return obj;
+
+  const { specie, sex } = animal;
+  const animalFiltro = species.find((anml) => anml.name === specie);
+  if (sex) return animalFiltro.residents.filter((element) => element.sex === sex).length;
+
+  const anime = Object.values(animal).find((element) => typeof element === 'string');
+  return obj[anime];
 }
 
 module.exports = countAnimals;
